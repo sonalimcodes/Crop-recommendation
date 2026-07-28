75f5304bb35dc0553b7b767a377e8f4329841553
Sonali M <sonalim24365@gmail.com>
2026-07-28 18:39:43 +0530

    Create database and users table in database.py

---
+++ database.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+def create_db():
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+create_db()
