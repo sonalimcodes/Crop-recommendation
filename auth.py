9aa3690cb593f0b38e6254def416f308c50c716c
Sonali M <sonalim24365@gmail.com>
2026-07-28 18:46:21 +0530

    Implement password length check in register_user
    Added password length validation in user registration.

--- auth.py
+++ auth.py
@@ -7,18 +7,24 @@ def hash_password(password):
 
 
 def register_user(username, password):
+
+    if len(password) < 8:
+        return "Password must be at least 8 characters long."
+
     conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
 
     try:
         cursor.execute(
-            "INSERT INTO users(username,password) VALUES(?,?)",
+            "INSERT INTO users(username, password) VALUES(?, ?)",
             (username, hash_password(password))
         )
         conn.commit()
         return True
-    except:
-        return False
+
+    except sqlite3.IntegrityError:
+        return "Username already exists."
+
     finally:
         conn.close()
 
